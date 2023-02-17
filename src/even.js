9b22d0ce8c97aa50dbebe9118670ddd898a2623a
acfohegi <acfohegi@proton.me>
2023-02-17 22:23:28 +0400

    5: even-game

---
+++ src/even.js
@@ -0,0 +1,39 @@
+import readlineSync from 'readline-sync';
+
+const getNumber = () => Math.floor(Math.random() * 100) + 1;
+const isEven = (num) => {
+  if (num % 2 === 0) {
+    return true;
+  }
+  return false;
+};
+
+const getQuestion = (num) => `Answer "yes" if the number is even, otherwise answer "no".\nQuestion: ${num}\nYour answer: `;
+const getNotCorrect = (userAnswer, correctAnswer, name) => `'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'. \nLet's try again, ${name}!`;
+const correct = 'Correct!';
+const getWinMessage = (name) => `Congratulations, ${name}!`;
+
+const game = () => {
+  console.log('Welcome to the Brain Games!');
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
+
+  let correctCount = 0;
+
+  while (correctCount < 3) {
+    const number = getNumber();
+    const userAnswer = readlineSync.question(getQuestion(number));
+    const correctAnswer = isEven(number) ? 'yes' : 'no';
+    if (userAnswer === correctAnswer) {
+      correctCount += 1;
+      console.log(correct);
+    }
+    if (userAnswer !== correctAnswer) {
+      correctCount = 0;
+      console.log(getNotCorrect(userAnswer, correctAnswer, name));
+    }
+  }
+  console.log(getWinMessage(name));
+};
+
+export default game;
